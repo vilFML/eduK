57c3b5a308a0059635e93e754bc8ca7d0dcf7b78
vilFML <fernandezm.vicente@gmail.com>
2026-04-19 20:30:23 -0400

    avance Tarea1 Grafik

--- Grafica/Tarea1/Tarea1_template.py
+++ Grafica/Tarea1/Tarea1_template.py
@@ -6,6 +6,8 @@ from pyglet.gl import *
 from pyglet.app import run
 from pyglet import clock
 
+from OpenGL import GL
+
 import sys, os
 import numpy as np
 # la siguiente linea le dice a python que cuando busque librerías, busque en la carpeta actual
@@ -15,8 +17,12 @@ sys.path.append(os.path.dirname(os.path.dirname((os.path.dirname(__file__)))))
 # https://en.wikipedia.org/wiki/Queue_(abstract_data_type)
 from collections import deque
 
-# Global: acel de gravedad
+## Variables Globales ##
+#  acel de gravedad
 G = -1
+# tiempo
+TIME = 0
+
 
 # Controla la ventana y el paso del tiempo
 class Controller(Window):
@@ -68,47 +74,159 @@ class Particle():
 if __name__ == "__main__":
 
     controller = Controller(800, 800, "Tarea_1")
-
+    ## Cañones ##
     # Vertex Shd
-    vert_source = """
+    vert_source_cannon = """
     #version 330
 
     in vec3 position;
+    in vec3 colors;
+    in float intensity;
     in float ttl;
+
+    out vec3 fragColor;
+    out float fragIntensity;
     out float alpha;
 
     void main()
     {
+        alpha = 1.0;
         gl_PointSize = 15.0 * (ttl / 3.0);
-        gl_Position = vec4(position, 1.0);
-        alpha = ttl / 3.0;
+        fragColor = colors;
+        fragIntensity = intensity;
+        gl_Position = vec4(position, 1.0f);
     }
     """
 
     # Fragment Shd
-    frag_source = """
+    frag_source_cannon = """
     #version 330
 
+    in vec3 fragColor;
+    in float fragIntensity;
     in float alpha;
+
     out vec4 outColor;
 
     void main()
     {
-        outColor = vec4(1.0, 1.0, 1.0, alpha);
+        outColor = fragIntensity * vec4(fragColor, alpha);
+    }
+    """
+
+
+
+    ## Particulas ##
+    # Vertex Shd
+    vert_source_ammo = """
+    #version 330
+
+    in vec3 position;
+    in vec3 colors;
+    in float intensity;
+    in float ttl;
+
+    out vec3 fragColor;
+    out float fragIntensity;
+    out float alpha;
+
+    void main()
+    {
+        alpha = ttl / 3.0;
+        gl_PointSize = 15.0 * (ttl / 3.0);
+        fragColor = colors;
+        fragIntensity = intensity;
+        gl_Position = vec4(position, 1.0f);
+    }
+    """
+
+    # Fragment Shd
+    frag_source_ammo = """
+    #version 330
+
+    in vec3 fragColor;
+    in float fragIntensity;
+    in float alpha;
+
+    out vec4 outColor;
+
+    void main()
+    {
+        outColor = fragIntensity * vec4(fragColor, alpha);
     }
     """
 
     # Dos pipelines
-    pipelineIzq = ShaderProgram(Shader(vert_source, "vertex"), Shader(frag_source, "fragment"))
+    pipelineIzq = ShaderProgram(Shader(vert_source_cannon, "vertex"), Shader(frag_source_cannon, "fragment"))
     
-    pipelineDer = ShaderProgram(Shader(vert_source, "vertex"), Shader(frag_source, "fragment"))
+    pipelineDer = ShaderProgram(Shader(vert_source_cannon, "vertex"), Shader(frag_source_cannon, "fragment"))
 
-    # Defina las figuras para los cañones
+    pipelineAmmo = ShaderProgram(Shader(vert_source_ammo, "vertex"), Shader(frag_source_ammo, "fragment"))
+
+    ### Definiciones de Figuras ###
+    # Vertices
+    # Cañon Izquierdo
+    positionsIzq = np.array([
+        -1, -1, 0,
+        -1, -0.8, 0,
+        -0.8, -0.8, 0,
+        -0.8, -1, 0
+    ], dtype = np.float32)
+    # Cañon derecho
+    positionsDer = np.array([
+        1, -1, 0,
+        0.8, -1, 0,
+        0.8, -0.8, 0,
+        1, -0.8 ,0
+    ], dtype= np.float32)
+
+
+    # Indices
+    indexIzq = np.array([
+        0, 1, 2,
+        2, 3, 0
+    ], dtype = np.uint32)
+
+    indexDer = np.array([
+        0, 1, 2,
+        2, 3, 0
+    ], dtype = np.uint32)
+
+    # Colores
+    colors = np.array([
+        1,1,1,
+        1,1,1,
+        1,1,1,
+        1,1,1
+    ], dtype = np.float32)
+
+    # Intensidad vértices
+    intensities = np.array([
+        1,
+        1,
+        1,
+        1
+    ], dtype = np.float32)
+
+    # Definicion de las figuras
+    # Cañones
+    gpu_cannonIzq = pipelineIzq.vertex_list_indexed(4, GL.GL_TRIANGLES, indexIzq)
+    gpu_cannonIzq.position = positionsIzq
+    gpu_cannonIzq.colors = colors
+    gpu_cannonIzq.intensity = intensities
+
+    gpu_cannonDer = pipelineDer.vertex_list_indexed(4, GL.GL_TRIANGLES, indexDer)
+    gpu_cannonDer.position = positionsDer
+    gpu_cannonDer.colors = colors
+    gpu_cannonDer.intensity = intensities
+
+    # Municion
+    gpu_ammoIzq = pipelineAmmo
     
 
     @controller.event
     def on_draw():
-        # Limpia pantalla y la coloca en el color determinado
+        # Limpia pantalla y la coloca en el color determinado (no la modifico)
         glClearColor(0.8,0.9,1,1)
         controller.clear()
 
@@ -118,49 +236,73 @@ if __name__ == "__main__":
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         
 
-        # Aquí debe selecicionar el pipeline que usara y luego dibujar sus cosas...
+        # Seleccionar pipeline y dibujar
         pipelineIzq.use()
+        gpu_cannonIzq.draw(GL.GL_TRIANGLES)
+
+        pipelineDer.use()
+        gpu_cannonDer.draw(GL.GL_TRIANGLES)
 
         if controller.particles_gpu_object is not None:
             controller.particles_gpu_object.draw(GL_POINTS)
 
 
-
-
-    # Esta función es importante, ya que le ayudará a interactuar con el teclado.
-    # Actualmente, está programada para que cada vez que se presione "A", 
-    # se imprima "Presionaste A!"
     @controller.event
     def on_key_press(symbol, modifiers):
         if symbol == key.A:
             print("Presionaste A!")
             # Generar particula Izq
+            controller.particles.append(Particle(
+                np.array([-0.9,-0.9,0]),                                        # PosInicial
+                np.array([0.1,0.1,0]),                                          # VelInicial
+                1.0                                                             # TTL
+            ))
         
-        elif symbol == key,D:
+        elif symbol == key.D:
             print('Presionaste D :(')
             # Generar particula Der
-
-        # Use esta función de Pyglet para generar sus partículas.
-
-
-
+            controller.particles.append(Particle(
+                np.array([0.9, -0.9, 0]),
+                np.array([-0.1,0.1,0]),
+                1.0
+            ))
 
     # Aquí se actualiza todo el sistema de partículas
     def update_particle_system(dt, controller):
 
-        # Estudie el aux 3 para entender bien este paso.
-        controller.particles_gpu_object = pipelineIzq.vertex_list( len(controller.particles), GL_POINTS )
+        # Sacar particulas no 'vivas'
+        to_remove = 0
+        for i in range(len(controller.particles)):
+            p = controller.particles[i]
+            p.step(dt)
+            if not p.alive():
+                to_remove += 1
+
+        for i in range(to_remove):
+            controller.particles.popleft()
+
+        if controller.particles_gpu_object is not None:
+            controller.particles_gpu_object.delete()
+            controller.particles_gpu_object = None
+
+        # si hay partículas vivas, hay que copiarlas a la GPU
+        if len(controller.particles) > 0:
+            controller.particles_gpu_object = pipelineIzq.vertex_list(
+                len(controller.particles),
+                GL_POINTS
+                )
+            
+            pos = []
+            ttls = []
+            for p in controller.particles:
+                pos += p.position.tolist()
+                ttls.append(p.ttl)
+
+            controller.particles_gpu_object.position[:] = np.array(pos)
+            controller.particles_gpu_object.ttl[:] = np.array(ttls)
             
         pos = []
-        ttls = []
-        for p in controller.particles:
-            pos += p.position.tolist()
-            ttls.append(p.ttl)
-
-        controller.particles_gpu_object.position[:] = np.array(pos)
-        controller.particles_gpu_object.ttl[:] = np.array(ttls)
-
-        
+        ttls = []        
 
     clock.schedule(update_particle_system, controller)
     run()
