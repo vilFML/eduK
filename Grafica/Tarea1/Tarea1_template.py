ecdbe6f314286d5839299102d003bcb52b8c5718
vilFML <fernandezm.vicente@gmail.com>
2026-04-20 10:15:19 -0400

    progreso tarea1

--- Grafica/Tarea1/Tarea1_template.py
+++ Grafica/Tarea1/Tarea1_template.py
@@ -20,9 +20,6 @@ from collections import deque
 ## Variables Globales ##
 #  acel de gravedad
 G = -1
-# tiempo
-TIME = 0
-
 
 # Controla la ventana y el paso del tiempo
 class Controller(Window):
@@ -38,34 +35,39 @@ class Controller(Window):
         self.time = 0
 
         # aplicando el 'deque' para las particulas
-        self.particles = deque()
-        self.particles_gpu_object = None
+        self.particlesLight = deque()
+        self.particlesLight_gpu_object = None
+
+        self.particlesHeavy = deque()
+        self.particlesHeavy_gpu_object = None
 
 
 
-# Se define el objeto particula
+# Objeto particula
 class Particle():
 
-    # ¿Qué debe otros parámetros son necesarios para crear una partícula?
-    def __init__(self, position, velocity, ttl):
+    def __init__(self, position, velocity, masa, ttl):
+
         self.position = np.array(position, dtype=np.float32)
 
-        #voy a usar vel cte por mientras
-        self.velocity = np.array( [np.random.uniform(-1,1),
-                                    np.random.uniform(-1,1),
-                                    0], dtype = np.float32 )
+        self.velocity = np.array(velocity, dtype = np.float32)
+
+        self.masa = masa
         
         self.ttl = ttl
 
-    # ¿Qué se debe actualizar en cada frame?
+    # Cada frame se actualiza la posicion, velocidad (si tiene masa) y tu ttl
     def step(self, dt):
-        # por un lado se reduce el tiempo de vida de la partícula
+        # se reduce el ttl
         self.ttl -= dt
 
-        # se actualiza la posición de la partícula
+        # actualiza la pos
         self.position += self.velocity * dt
 
-        self.velocity[1] = self.velocity[1] + G * dt
+        # actualizar vel si tiene masa, si no: cte
+        if self.masa == 1:
+            self.velocity[1] = self.velocity[1] + G * dt
+
 
     # Funcion que entrega si particula esta viva
     def alive(self):
@@ -97,7 +99,6 @@ if __name__ == "__main__":
         gl_Position = vec4(position, 1.0f);
     }
     """
-
     # Fragment Shd
     frag_source_cannon = """
     #version 330
@@ -114,10 +115,53 @@ if __name__ == "__main__":
     }
     """
 
+    ## Municion ##
+    # Vertex Shd
+    vert_source_ammo = """
+    #version 330
+
+    uniform float u_ttl_max;
+
+    in vec3 position;
+    in float intensity;
+    in float ttl;
+
+    vec4 colorA = vec4(0.0, 0.0, 1.0, 1.0);
+    vec4 colorB = vec4(1.0, 0.0, 0.0, 1.0);
+
+    out vec4 fragColor;
+    out float fragIntensity;
+
+    void main()
+    {
+        float pct = 1.0 - (ttl / u_ttl_max);
+        vec4 color = mix(colorA, colorB, pct);
+
+        gl_PointSize = 30.0 * (ttl / u_ttl_max);
+        fragColor = color;
+        fragIntensity = intensity;
+        gl_Position = vec4(position, 1.0f);
+    }
+    """
+
+    # Fragment Shd
+    frag_source_ammo = """
+    #version 330
+
+    in vec4 fragColor;
+    in float fragIntensity;
+    out vec4 outColor;
+
+    void main()
+    {
+        outColor = fragColor;
+    }
+    """
+
     # Dos pipelines
-    pipelineIzq = ShaderProgram(Shader(vert_source_cannon, "vertex"), Shader(frag_source_cannon, "fragment"))
+    pipelineCannon = ShaderProgram(Shader(vert_source_cannon, "vertex"), Shader(frag_source_cannon, "fragment"))
     
-    pipelineDer = ShaderProgram(Shader(vert_source_cannon, "vertex"), Shader(frag_source_cannon, "fragment"))
+    pipelineAmmo = ShaderProgram(Shader(vert_source_ammo, "vertex"), Shader(frag_source_ammo, "fragment"))
 
     ### Definiciones de Figuras ###
     # Vertices
@@ -165,19 +209,17 @@ if __name__ == "__main__":
     ], dtype = np.float32)
 
     # Definicion de las figuras
-    # Cañones
-    gpu_cannonIzq = pipelineIzq.vertex_list_indexed(4, GL.GL_TRIANGLES, indexIzq)
+    # Cañon izq
+    gpu_cannonIzq = pipelineCannon.vertex_list_indexed(4, GL.GL_TRIANGLES, indexIzq)
     gpu_cannonIzq.position = positionsIzq
     gpu_cannonIzq.colors = colors
     gpu_cannonIzq.intensity = intensities
 
-    gpu_cannonDer = pipelineDer.vertex_list_indexed(4, GL.GL_TRIANGLES, indexDer)
+    # Cañon der
+    gpu_cannonDer = pipelineCannon.vertex_list_indexed(4, GL.GL_TRIANGLES, indexDer)
     gpu_cannonDer.position = positionsDer
     gpu_cannonDer.colors = colors
     gpu_cannonDer.intensity = intensities
-
-    # Municion
-    
 
     @controller.event
     def on_draw():
@@ -192,14 +234,19 @@ if __name__ == "__main__":
         
 
         # Seleccionar pipeline y dibujar
-        pipelineIzq.use()
+        pipelineCannon.use()
         gpu_cannonIzq.draw(GL.GL_TRIANGLES)
-
-        pipelineDer.use()
         gpu_cannonDer.draw(GL.GL_TRIANGLES)
 
-        if controller.particles_gpu_object is not None:
-            controller.particles_gpu_object.draw(GL_POINTS)
+        if controller.particlesLight_gpu_object is not None:
+            pipelineAmmo.use()
+            pipelineAmmo['u_ttl_max'] = 2.0
+            controller.particlesLight_gpu_object.draw(GL_POINTS)
+        if controller.particlesHeavy_gpu_object is not None:
+            pipelineAmmo.use()
+            pipelineAmmo['u_ttl_max'] = 5.0
+            controller.particlesHeavy_gpu_object.draw(GL_POINTS)
+
 
 
     @controller.event
@@ -207,57 +254,100 @@ if __name__ == "__main__":
         if symbol == key.A:
             print("Presionaste A!")
             # Generar particula Izq
-            controller.particles.append(Particle(
+            controller.particlesHeavy.append(Particle(
                 np.array([-0.9,-0.9,0]),                                        # PosInicial
-                np.array([0.1,0.1,0]),                                          # VelInicial
+                np.array([np.random.uniform(0.8,1.0),np.random.uniform(0.8,1.0),0]),# VelInicial
+                1,                                                              # si tiene masa o no
                 1.0                                                             # TTL
             ))
         
         elif symbol == key.D:
             print('Presionaste D :(')
             # Generar particula Der
-            controller.particles.append(Particle(
+            controller.particlesLight.append(Particle(
                 np.array([0.9, -0.9, 0]),
-                np.array([-0.1,0.1,0]),
-                1.0
+                np.array([np.random.uniform(-0.8,-1.0),np.random.uniform(0.8,1.0),0]),
+                0,
+                2.0                                                             # ttl mas larga
             ))
 
     # Aquí se actualiza todo el sistema de partículas
     def update_particle_system(dt, controller):
 
-        # Sacar particulas no 'vivas'
-        to_remove = 0
-        for i in range(len(controller.particles)):
-            p = controller.particles[i]
+        # Sacar particulas 'no vivas'
+        to_removeHeavy = 0
+        to_removeLight = 0
+        
+        for i in range(len(controller.particlesHeavy)):
+            p = controller.particlesHeavy[i]
             p.step(dt)
             if not p.alive():
-                to_remove += 1
+                to_removeHeavy += 1
 
-        for i in range(to_remove):
-            controller.particles.popleft()
+        for i in range(to_removeHeavy):
+            controller.particlesHeavy.popleft()
 
-        if controller.particles_gpu_object is not None:
-            controller.particles_gpu_object.delete()
-            controller.particles_gpu_object = None
+
+        for i in range(len(controller.particlesLight)):
+            p = controller.particlesLight[i]
+            p.step(dt)
+            if not p.alive():
+                to_removeLight += 1
+
+        for i in range(to_removeLight):
+            controller.particlesLight.popleft()
+
+
+        if (controller.particlesLight_gpu_object and controller.particlesHeavy_gpu_object)  is not None:
+            controller.particlesLight_gpu_object.delete()
+            controller.particlesHeavy_gpu_object.delete()
+            controller.particlesLight_gpu_object = None
+            controller.particlesHeavy_gpu_object = None
 
         # si hay partículas vivas, hay que copiarlas a la GPU
-        if len(controller.particles) > 0:
-            controller.particles_gpu_object = pipelineIzq.vertex_list(
-                len(controller.particles),
+        # mnunición con masa
+        if len(controller.particlesHeavy) > 0:
+            controller.particlesHeavy_gpu_object = pipelineAmmo.vertex_list(
+                len(controller.particlesHeavy),
                 GL_POINTS
                 )
             
-            pos = []
-            ttls = []
-            for p in controller.particles:
-                pos += p.position.tolist()
-                ttls.append(p.ttl)
+            posHeavy = []
+            ttlsHeavy = []
+            colsHeavy = []
+            intesHeavy = []
+            for p in controller.particlesHeavy:
+                posHeavy += p.position.tolist()
+                ttlsHeavy.append(p.ttl)
+                colsHeavy += [0, 0, 1.0]
+                intesHeavy.append(1.0)
 
-            controller.particles_gpu_object.position[:] = np.array(pos)
-            controller.particles_gpu_object.ttl[:] = np.array(ttls)
+            controller.particlesHeavy_gpu_object.position[:] = np.array(posHeavy, dtype=np.float32)
+            controller.particlesHeavy_gpu_object.ttl[:] = np.array(ttlsHeavy, dtype=np.float32)
+#            controller.particlesHeavy_gpu_object.colors[:] = np.array(colsHeavy, dtype=np.float32)
+            controller.particlesHeavy_gpu_object.intensity[:] = np.array(intesHeavy, dtype = np.float32)
+
+
+        # municion sin masa
+        if len(controller.particlesLight) > 0:
+            controller.particlesLight_gpu_object = pipelineCannon.vertex_list(
+                len(controller.particlesLight),
+                GL_POINTS)
             
-        pos = []
-        ttls = []        
+            posLight = []
+            ttlsLight = []
+            colsLight = []
+            intesLight = []
+            for p in controller.particlesLight:
+                posLight += p.position.tolist()
+                ttlsLight.append(p.ttl)
+                colsLight += [0, 0, 1.0]
+                intesLight.append(1.0)
+
+            controller.particlesLight_gpu_object.position[:] = np.array(posLight, dtype=np.float32)
+            controller.particlesLight_gpu_object.ttl[:] = np.array(ttlsLight, dtype=np.float32)
+#            controller.particlesLight_gpu_object.colors[:] = np.array(colsLight, dtype=np.float32)
+            controller.particlesLight_gpu_object.intensity[:] = np.array(intesLight, dtype = np.float32)
 
     clock.schedule(update_particle_system, controller)
     run()
